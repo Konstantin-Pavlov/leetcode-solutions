69030a8e9fcbbd97f4a59abb901a64319652d04d
Konstantin-Pavlov <konstpavlov87@gmail.com>
2024-04-24 12:35:53 +0600

    fixed regex

--- src/main/java/easy/ValidPalindrome.java
+++ src/main/java/easy/ValidPalindrome.java
@@ -2,7 +2,7 @@ package easy;
 
 public class ValidPalindrome {
     public boolean isPalindrome(String s) {
-        StringBuilder sb = new StringBuilder(s.toLowerCase().replaceAll("[^a-zA-Z]", ""));
-        return sb.toString().contentEquals(sb.reverse());
+        StringBuilder sb = new StringBuilder(s.toLowerCase().replaceAll("[^a-z0-9]", ""));
+        return sb.toString().contentEquals(sb.reverse().toString());
     }
 }
