59720f5deb9a4f3699e465a179a7806a099b3fcc
Konstantin-Pavlov <konstpavlov87@gmail.com>
2024-04-26 18:27:34 +0600

    add new solution to problem - BackspaceStringCompare

--- src/main/java/easy/BackspaceStringCompare.java
+++ src/main/java/easy/BackspaceStringCompare.java
@@ -9,6 +9,26 @@ public class BackspaceStringCompare {
         return sBuilder.toString().contentEquals(tBuilder);
     }
 
+    public static boolean backspaceCompareWithOneFor(String s, String t) {
+        StringBuilder sBuilder = new StringBuilder();
+        StringBuilder tBuilder = new StringBuilder();
+        int sCounter = s.length() - 1;
+        int tCounter = t.length() - 1;
+        int sSkip = 0;
+        int tSkip = 0;
+        while (sCounter > -1 || tCounter > -1) {
+            if (sCounter > -1) {
+                sSkip = updateBuilderAndGetSkip(s, sBuilder, sCounter, sSkip);
+                sCounter--;
+            }
+            if (tCounter > -1) {
+                tSkip = updateBuilderAndGetSkip(t, tBuilder, tCounter, tSkip);
+                tCounter--;
+            }
+        }
+        return sBuilder.toString().contentEquals(tBuilder);
+    }
+
     private static void backspaceProcessing(String line, StringBuilder sBuilder) {
         int skip = 0;
         for (int i = line.length() - 1; i >= 0; i--) {
@@ -21,4 +41,16 @@ public class BackspaceStringCompare {
             }
         }
     }
+
+    private static int updateBuilderAndGetSkip(String line, StringBuilder lineBuilder, int index, int skip) {
+        if (line.charAt(index) == '#') {
+            skip++;
+        } else if (skip == 0) {
+            lineBuilder.append(line.charAt(index));
+        } else {
+            skip--;
+        }
+        return skip;
+    }
+
 }
--- src/test/java/easy/BackspaceStringCompareTest.java
+++ src/test/java/easy/BackspaceStringCompareTest.java
@@ -45,5 +45,61 @@ public class BackspaceStringCompareTest {
         String t = "xywrrmu#p";
         Assertions.assertTrue(BackspaceStringCompare.backspaceCompare(s, t));
     }
+
+    @Test
+    public void test7() {
+        String s = "bxj##tw";
+        String t = "bxj###tw";
+        Assertions.assertFalse(BackspaceStringCompare.backspaceCompare(s, t));
+    }
+
+    @Test
+    public void test8() {
+        String s = "ab#c";
+        String t = "ad#c";
+        Assertions.assertTrue(BackspaceStringCompare.backspaceCompareWithOneFor(s, t));
+    }
+
+    @Test
+    public void test9() {
+        String s = "ab##";
+        String t = "c#d#";
+        Assertions.assertTrue(BackspaceStringCompare.backspaceCompareWithOneFor(s, t));
+    }
+
+    @Test
+    public void test10() {
+        String s = "a#c";
+        String t = "b";
+        Assertions.assertFalse(BackspaceStringCompare.backspaceCompareWithOneFor(s, t));
+    }
+
+    @Test
+    public void test11() {
+        String s = "a##c";
+        String t = "#a#c";
+        Assertions.assertTrue(BackspaceStringCompare.backspaceCompareWithOneFor(s, t));
+    }
+
+    @Test
+    public void test12() {
+        String s = "y#fo##f";
+        String t = "y#f#o##f";
+        Assertions.assertTrue(BackspaceStringCompare.backspaceCompareWithOneFor(s, t));
+    }
+
+    @Test
+    public void test13() {
+        String s = "xywrrmp";
+        String t = "xywrrmu#p";
+        Assertions.assertTrue(BackspaceStringCompare.backspaceCompareWithOneFor(s, t));
+    }
+
+    @Test
+    public void test14() {
+        String s = "bxj##tw";
+        String t = "bxj###tw";
+        Assertions.assertFalse(BackspaceStringCompare.backspaceCompareWithOneFor(s, t));
+    }
 }
 
