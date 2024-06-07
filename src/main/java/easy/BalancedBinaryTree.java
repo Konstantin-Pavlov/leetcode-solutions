a141a1abd731f06892ad5bbac31fb6d9cfb9565a
Konstantin-Pavlov <konstpavlov87@gmail.com>
2024-06-07 17:22:26 +0600

    add BalancedBinaryTree problem

---
+++ src/main/java/easy/BalancedBinaryTree.java
@@ -0,0 +1,23 @@
+package easy;
+
+// https://leetcode.com/problems/balanced-binary-tree/
+public class BalancedBinaryTree {
+    public boolean isBalanced(TreeNode root) {
+        return height(root) != -1;
+    }
+
+    private int height(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+
+        int leftHeight = height(node.left);
+        int rightHeight = height(node.right);
+
+        if (leftHeight == -1 || rightHeight == -1 || Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        }
+
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+}
